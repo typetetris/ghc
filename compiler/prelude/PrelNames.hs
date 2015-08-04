d4dc980ca607d3d26b26a1a301c37ac93546263a
Ben Gamari <ben@smart-cactus.org>
2015-08-04 09:07:11 +0200

    Implement cheapBuild

--- compiler/prelude/PrelNames.hs
+++ compiler/prelude/PrelNames.hs
@@ -277,7 +277,7 @@ basicKnownKeyNames
 
         -- List operations
         concatName, filterName, mapName,
-        zipName, foldrName, buildName, augmentName, appendName,
+        zipName, foldrName, buildName, cheapBuildName, augmentName, appendName,
 
         -- FFI primitive types that are not wired-in.
         stablePtrTyConName, ptrTyConName, funPtrTyConName,
@@ -872,7 +872,8 @@ groupWithName :: Name
 groupWithName = varQual gHC_EXTS (fsLit "groupWith") groupWithIdKey
 
 -- Random PrelBase functions
-fromStringName, otherwiseIdName, foldrName, buildName, augmentName,
+fromStringName, otherwiseIdName, foldrName,
+    buildName, cheapBuildName, augmentName,
     mapName, appendName, assertName,
     breakpointName, breakpointCondName, breakpointAutoName,
     opaqueTyConName :: Name
@@ -880,6 +881,7 @@ fromStringName = varQual dATA_STRING (fsLit "fromString") fromStringClassOpKey
 otherwiseIdName   = varQual gHC_BASE (fsLit "otherwise")  otherwiseIdKey
 foldrName         = varQual gHC_BASE (fsLit "foldr")      foldrIdKey
 buildName         = varQual gHC_BASE (fsLit "build")      buildIdKey
+cheapBuildName    = varQual gHC_BASE (fsLit "cheapBuild") cheapBuildIdKey
 augmentName       = varQual gHC_BASE (fsLit "augment")    augmentIdKey
 mapName           = varQual gHC_BASE (fsLit "map")        mapIdKey
 appendName        = varQual gHC_BASE (fsLit "++")         appendIdKey
@@ -1625,7 +1627,7 @@ srcLocDataConKey                        = mkPreludeDataConUnique 37
 -}
 
 wildCardKey, absentErrorIdKey, augmentIdKey, appendIdKey,
-    buildIdKey, errorIdKey, foldrIdKey, recSelErrorIdKey,
+    buildIdKey, cheapBuildIdKey, errorIdKey, foldrIdKey, recSelErrorIdKey,
     seqIdKey, irrefutPatErrorIdKey, eqStringIdKey,
     noMethodBindingErrorIdKey, nonExhaustiveGuardsErrorIdKey,
     runtimeErrorIdKey, patErrorIdKey, voidPrimIdKey,
@@ -1657,6 +1659,7 @@ unpackCStringFoldrIdKey       = mkPreludeMiscIdUnique 19
 unpackCStringIdKey            = mkPreludeMiscIdUnique 20
 voidPrimIdKey                 = mkPreludeMiscIdUnique 21
 typeErrorIdKey                = mkPreludeMiscIdUnique 22
+cheapBuildIdKey               = mkPreludeMiscIdUnique 23
 
 unsafeCoerceIdKey, concatIdKey, filterIdKey, zipIdKey, bindIOIdKey,
     returnIOIdKey, newStablePtrIdKey,
