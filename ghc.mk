2ddb024b61237b1a9e5c4f9305be1a3776dd9476
Ben Gamari <ben@smart-cactus.org>
2018-05-30 22:47:48 -0400

    fixes

--- ghc.mk
+++ ghc.mk
@@ -462,6 +462,7 @@ PACKAGES_STAGE1 += ghc-boot-th
 PACKAGES_STAGE1 += ghc-boot
 PACKAGES_STAGE1 += template-haskell
 PACKAGES_STAGE1 += ghc-compact
+PACKAGES_STAGE1 += ghc-heap
 PACKAGES_STAGE1 += prettyprinter/prettyprinter
 
 ifeq "$(HADDOCK_DOCS)" "YES"
